583d96f6712c7690882cb771fdd07d067134b64b
RInRInPark <surinp8019@naver.com>
2022-10-08 16:58:49 +0900

    2주차 세미나

---
+++ seminar2-assignment/src/api/comment.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response, Router } from "express";
+
+const router: Router = express.Router();
+
+router.get("/", (req: Request, res: Response) => {
+  return res.status(200).json({
+    status: 200,
+    message: "comment 라우터지렁이",
+  });
+});
+
+module.exports = router;
